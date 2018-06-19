a5649f98196fef22e312a7383407f0a41fbe1a4d
dthigpen <davidthigs@gmail.com>
2018-06-18 19:30:03 -0500

    add support for like/dislike

--- gpmdp.py
+++ gpmdp.py
@@ -4,8 +4,8 @@ import json
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p','--path',help='the path to the .json file containing playback info')
-parser.add_argument('-f','--format',help='how to output the playback information. ex: "{A} - {T}" | A = artist, a = album, T = title, l = elaped, L = length')
+parser.add_argument('-p','--path',help='the path to the .json file containing playback info.')
+parser.add_argument('-f','--format',help='how to output the playback information. ex: "{A} - {T}" | A = artist, a = album, T = title, l = elaped, L = length, r = liked, R = disliked')
 args = parser.parse_args()
 
 if args.path:
@@ -19,7 +19,10 @@ with open(filepath) as f:
         elapsed = str(datetime.timedelta(seconds=int(data['time']['current']))//1000)
         length = str(datetime.timedelta(seconds=int(data['time']['total']))//1000)
         if args.format:
-            print(args.format.format(A=data['song']['artist'],a=data['song']['album'],T=data['song']['title'],l=elapsed,L=length))
+            print(args.format.format(A=data['song']['artist'], \
+            a=data['song']['album'],T=data['song']['title'], \
+            l=elapsed,L=length,r=data['rating']['liked'], \ 
+            R=data['rating']['disliked']))
         else:
             print(data['song']['artist'],'-',data['song']['title'])
     
